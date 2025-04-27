4aebcec473fb042a3b50030cbcab0ea12453a979
ghoulyaev <ghoulyaev@gmail.com>
2025-04-27 18:53:42 +0300

    fix log file

--- src/logging/logging.ts
+++ src/logging/logging.ts
@@ -4,5 +4,5 @@ export const showLog = (log: string): void => {
   const logDate: Date = new Date();
   const logString: string = `[${logDate}]: ${log}`;
   console.log(logString);
-  appendFileSync("./log.txt/", `${logString}\n`);
+  appendFileSync("log.txt", `${logString}\n`);
 };
