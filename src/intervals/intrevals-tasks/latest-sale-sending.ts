b37bc9a940f500eeb1b498697dae70631dcd49d9
ghoulyaev <ghoulyaev@gmail.com>
2025-04-28 19:59:52 +0300

    fix latestSale interval

--- src/intervals/intrevals-tasks/latest-sale-sending.ts
+++ src/intervals/intrevals-tasks/latest-sale-sending.ts
@@ -1,7 +1,6 @@
 import { Telegraf } from "telegraf";
 import { readData, writeData } from "../../data-manager";
 import { showLog } from "../../logging/logging";
-import { getLatestSaleRequestOptions } from "../../requests/latest-sale-request-options";
 import { TonnelApi } from "../../api/tonnel-api";
 
 const buildNewSaleText = (gift: any, date: Date): string => {
@@ -16,36 +15,37 @@ const buildNewSaleText = (gift: any, date: Date): string => {
   }:${date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()}`;
 };
 
-export const latestSaleSending = (app: Telegraf, tonnel: TonnelApi): void => {
+export const latestSaleSending = async (
+  app: Telegraf,
+  tonnel: TonnelApi
+): Promise<void> => {
   const data: IData = readData();
-  tonnel
-    .getLatestSale()
-    .then((gift) => {
-      const date: Date = new Date(gift.timestamp);
-      if (gift.timestamp != data.latestSale.time) {
-        data.latestSale.time = gift.timestamp;
-        writeData(data);
-        if (data.chatsId.length != 0) {
-          data.chatsId.forEach((id) => {
-            app.telegram
-              .sendMessage(id, buildNewSaleText(gift, date), {
-                parse_mode: "HTML",
-              })
-              .then(() => {
-                showLog(`Sent a message about the new sale to chat ${id}`);
-              })
-              .catch((error) => {
-                showLog(`${error.toString()} in latestSaleSending`);
-                const index = data.chatsId.indexOf(id);
-                if (index != -1) {
-                  data.chatsId.splice(index, 1);
-                }
-              });
-          });
-        }
+  try {
+    const gift = await tonnel.getLatestSale();
+    const date: Date = new Date(gift.timestamp);
+    if (gift.timestamp != data.latestSale.time) {
+      data.latestSale.time = gift.timestamp;
+      writeData(data);
+      if (data.chatsId.length != 0) {
+        data.chatsId.forEach((id) => {
+          app.telegram
+            .sendMessage(id, buildNewSaleText(gift, date), {
+              parse_mode: "HTML",
+            })
+            .then(() => {
+              showLog(`Sent a message about the new sale to chat ${id}`);
+            })
+            .catch((error) => {
+              showLog(`${error.toString()} in latestSaleSending`);
+              const index = data.chatsId.indexOf(id);
+              if (index != -1) {
+                data.chatsId.splice(index, 1);
+              }
+            });
+        });
       }
-    })
-    .catch((error) => {
-      `${error.toString()}`;
-    });
+    }
+  } catch (error: any) {
+    showLog(`${error.toString()}`);
+  }
 };
