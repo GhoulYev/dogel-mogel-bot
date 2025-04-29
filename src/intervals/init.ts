00dde64307166b8dcf57bd1d1340e0262fa566a8
ghoulyaev <ghoulyaev@gmail.com>
2025-04-29 21:25:29 +0300

    update interval time

--- src/intervals/init.ts
+++ src/intervals/init.ts
@@ -3,7 +3,7 @@ import { priceSending } from "./intrevals-tasks/floor-sending";
 import { latestSaleSending } from "./intrevals-tasks/latest-sale-sending";
 import { TonnelApi } from "../api/tonnel-api";
 
-const floorInterval = 1800000;
+const floorInterval = 7200000;
 const latestSaleInterval = 10000;
 export const initIntervals = (app: Telegraf, tonnel: TonnelApi): void => {
   setInterval(() => priceSending(app, tonnel), floorInterval);
